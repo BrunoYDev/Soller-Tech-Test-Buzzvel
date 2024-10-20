273f2ad7613c7c05f0809946c02611b13039a983
Bruno Rodrigues Garcia <118909147+BrunoYDev@users.noreply.github.com>
2024-10-20 17:42:28 -0300

    Add: Second Section

--- src/Components/SecondSection/SecondSection.jsx
+++ src/Components/SecondSection/SecondSection.jsx
@@ -1,17 +1,59 @@
-import React from "react";
-import { Container, StyledText, Subtitle, TextContainer, Title } from "./style";
+import React, { useEffect, useRef } from "react";
+import { Container, StyledFigure, StyledOrangeCircle, StyledPurpleCircle, StyledText, Subtitle, TextContainer, Title } from "./style";
+import desktopImage from "../../assets/Desktop.png"
+import gsap from "gsap";
 
 function SecondSection() {
+    const desktopRef = useRef(null);
+    const titleRef = useRef(null);
+    const subtitleRef = useRef(null);
+    const textRef = useRef(null);
+    const orangeCircle = useRef(null);
+    const purpleCircle = useRef(null);
+
+    useEffect(() => {
+        const fadeInElements = [desktopRef.current, titleRef.current, subtitleRef.current, textRef.current,orangeCircle.current,purpleCircle.current];
+
+        const observer = new IntersectionObserver((entries) => {
+            entries.forEach(entry => {
+                if (entry.isIntersecting) {
+                    gsap.fromTo(entry.target, 
+                        { opacity: 0, y: -150 }, 
+                        { opacity: 1, y: 0, duration: 0.5, ease: 'power3.out' }
+                    );
+                    observer.unobserve(entry.target);
+                }
+            });
+        }, { threshold: 0.5 });
+
+        fadeInElements.forEach(el => {
+            if (el) {
+                observer.observe(el);
+            }
+        });
+
+        return () => {
+            fadeInElements.forEach(el => {
+                if (el) {
+                    observer.unobserve(el);
+                }
+            });
+        };
+    }, []);
 
   return (
     <Container>
+    <StyledOrangeCircle ref={orangeCircle}></StyledOrangeCircle>
+    <StyledPurpleCircle ref={purpleCircle}></StyledPurpleCircle>
       <TextContainer>
-        <Title>No more waste</Title>
-        <Subtitle>Pick the Sun</Subtitle>
-        <StyledText>Et pulvinar nec interdum integer id urna molestie porta nullam. A, donec ornare sed turpis pulvinar purus maecenas quam a. Erat porttitor pharetra sed in mauris elementum sollicitudin.</StyledText>
+        <Title ref={titleRef}>No more waste</Title>
+        <Subtitle ref={subtitleRef}>Pick the Sun</Subtitle>
+        <StyledText ref={textRef}>Et pulvinar nec interdum integer id urna molestie porta nullam. A, donec ornare sed turpis pulvinar purus maecenas quam a. Erat porttitor pharetra sed in mauris elementum sollicitudin.</StyledText>
       </TextContainer>
 
-      
+      <StyledFigure ref={desktopRef}>
+        <img src={desktopImage} alt="Desktop Image" />
+      </StyledFigure>
     </Container>
   )
 }
--- src/Components/SecondSection/style.js
+++ src/Components/SecondSection/style.js
@@ -4,6 +4,9 @@ export const Container = styled.section`
 
     margin-top: 50px;
     display: flex;
+    position: relative;
+    width: 100vw;
+    overflow: hidden;
 
 /* Mobile (until 768px) */
 @media (max-width: 768px) {
@@ -46,6 +49,45 @@ export const StyledText = styled.p`
 
 export const StyledFigure = styled.figure`
 
-    
 
+/* Mobile (until 768px) */
+@media (max-width: 768px) {
+    margin: 35px 0 0 0;
+    padding: 5px;
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    object-fit: cover;
+}
+`;
+
+export const StyledOrangeCircle = styled.div`
+
+/* Mobile (until 768px) */
+@media (max-width: 768px) {
+    background-color: #F59E0B;
+    width: 250px;
+    height: 250px;
+    border-radius: 100%;
+    position: absolute;
+    z-index: -1;
+    left: -180px;
+    top: 180px;
+}
+`;
+
+export const StyledPurpleCircle = styled.div`
+
+/* Mobile (until 768px) */
+@media (max-width: 768px) {
+    background-color: #7E22CE;
+    width: 210px;
+    height: 210px;
+    border-radius: 100%;
+    position: absolute;
+    z-index: -1;
+    right: -170px;
+    top: 220px;
+}
 `;
