2e87731a9d842f840f877eca3eac4a63d2485145
Bruno Rodrigues Garcia <118909147+BrunoYDev@users.noreply.github.com>
2024-10-21 22:12:07 -0300

    Add: FifthSection to app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import FirstSection from "./Components/FirstSection/FirstSection";
 import SecondSection from "./Components/SecondSection/SecondSection";
 import ThirdSection from "./Components/ThirdSection/ThirdSection";
 import FourthSection from "./Components/FourthSection/FourthSection";
+import FifthSection from "./Components/FifthSection/FifthSection";
 
 function App() {
 
@@ -14,6 +15,7 @@ function App() {
       <SecondSection/>
       <ThirdSection/>
       <FourthSection/>
+      <FifthSection/>
     </>
   )
 }
