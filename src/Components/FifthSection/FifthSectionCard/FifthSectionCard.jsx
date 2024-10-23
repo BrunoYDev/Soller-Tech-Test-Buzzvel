a21059c8200a2bdd551bd02f65c0b3574d1f0770
Bruno Rodrigues Garcia <118909147+BrunoYDev@users.noreply.github.com>
2024-10-23 16:29:19 -0300

    Add: Desktop FifthSectionCard Design

--- src/Components/FifthSection/FifthSectionCard/FifthSectionCard.jsx
+++ src/Components/FifthSection/FifthSectionCard/FifthSectionCard.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import { Container, Desc, Name, Text, TextContainer, UserField } from "./style";
 
-function FifthSectionCard({opinion}) {
+function FifthSectionCard({opinion, $isActive}) {
 
     
       return (
-        <Container>
+        <Container $isActive={$isActive}>
             <Text>{opinion.text}</Text>
             <UserField>
             <img src={opinion.pic} alt="profile picture" />
--- src/Components/FifthSection/FifthSectionCard/style.js
+++ src/Components/FifthSection/FifthSectionCard/style.js
@@ -1,73 +1,82 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
+  background-color: #fff;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  border-radius: 20px;
+  width: 300px;
+  height: 350px;
+  margin-left: 0;
+  margin-right: 0;
+  margin-top: 50px;
+  margin-bottom: 150px;
+  padding: 30px;
 
-    background-color: #fff;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    border-radius: 20px;
+  transition: transform 0.3s ease;
 
-/* Mobile (until 768px) */
+  ${({ $isActive }) => $isActive && `
+    transform: scale(1,1.05); 
+    z-index: 1;
+  `}
+  /* Mobile (until 768px) */
   @media (max-width: 768px) {
-    width: 300px;
-    height: 350px;
-    
-    margin: 0 auto;
-    margin-top: 50px;
-    margin-bottom: 150px;
-    padding: 30px;
+    margin-left: 20px;
+    margin-right: 0px;
   }
 
+  /* Desktop (above 1024px) */
+  @media (min-width: 1024px) {
+    margin-left: 0px;
+    margin-right: 15px;
+  }
 `;
 
 export const Name = styled.h2`
-
-    margin: 0;
-    padding: 0;
-    font-weight: 400;
-    font-size: 18px;
-
+  margin: 0;
+  padding: 0;
+  font-weight: 400;
+  font-size: 18px;
 `;
 
 export const Desc = styled.h3`
-    margin: 0;
-    padding: 0;
-    font-weight: 400;
-    font-size: 16px;
-
+  margin: 0;
+  padding: 0;
+  font-weight: 400;
+  font-size: 16px;
 `;
 
 export const Text = styled.p`
-
-    margin-top: 50px;
-    font-weight: 400;
-    font-size: 18px;
-
+  margin-top: 50px;
+  font-weight: 400;
+  font-size: 18px;
 `;
 
 export const UserField = styled.div`
+  display: flex;
+  width: 100%;
+  margin-top: 50px;
+  align-items: start;
+  gap: 20px;
 
-    display: flex;
-    width: 100%;
-    margin-top: 50px;
-    align-items: start;
-    gap: 20px;
+  img {
+    width: 64px;
+    height: 64px;
+    border-radius: 100%;
+    object-fit: cover;
+  }
 
-
-    img{
-        width: 64px;
-        height: 64px;
-        border-radius: 100%;
-        object-fit: cover;
+  /* Desktop (above 1024px) */
+  @media (min-width: 1024px) {
+    img {
+      object-fit: cover;
     }
-
+  }
 `;
 
 export const TextContainer = styled.div`
-
-    display: flex;
-    flex-direction: column;
-
+  display: flex;
+  flex-direction: column;
 `;
