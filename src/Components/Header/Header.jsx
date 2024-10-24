e7d543792d67a5f9ade903e774e70a7fc6738609
Bruno Rodrigues Garcia <118909147+BrunoYDev@users.noreply.github.com>
2024-10-24 11:18:44 -0300

    Update: React Helmet meta tags

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -28,7 +28,11 @@ function Header() {
           <title>Get the Sun to Power Your Home - Soller</title>
           <meta name="description" content="Discover how solar power can help you save energy and power your home efficiently." />
           <meta property="og:title" content="Get the Sun to Power Your Home" />
+          <meta name="description" content="Discover how solar power can help you save energy and power your home efficiently." />
           <meta property="og:description" content="Discover how solar power can help you save energy." />
+          <meta property="og:image" content={ContactLogo} />
+          <meta property="og:site_name" content="Soller" />
+          <meta property="og:url" content="https://soller-solar-company.vercel.app" />
         </Helmet>
         <StyledHeader>
           <StyledNav>
