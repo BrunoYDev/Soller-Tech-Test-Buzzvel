4476a2bf9b29b1ebcf95cafa5c5a0cfd3c25a7b4
Bruno Rodrigues Garcia <118909147+BrunoYDev@users.noreply.github.com>
2024-10-24 12:06:49 -0300

    Add: Static Site Generation(SSG)

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import { ViteReactSSG } from 'vite-react-ssg/single-page';
 import './index.css'
+
+export const createRootElement = ViteReactSSG(<App />);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    {createRootElement}
   </StrictMode>,
 )
