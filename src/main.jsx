261f261b54811ab57c72f0c8335f204b6955dbb3
Bruno Rodrigues Garcia <118909147+BrunoYDev@users.noreply.github.com>
2024-10-24 12:28:39 -0300

    Fix: main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-import { ViteReactSSG } from 'vite-react-ssg/single-page';
 import './index.css'
-
-export const createRootElement = ViteReactSSG(<App />);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    {createRootElement}
+    <App />
   </StrictMode>,
 )
